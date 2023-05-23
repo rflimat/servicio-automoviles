fcd12d57d8e7c6269d1f0c4db97c12396e965527
kgmamanic <kgmamanic@unjbg.edu.pe>
2023-05-22 19:14:38 -0500

    Ventas avanzado

---
+++ api/app/Http/Controllers/api/VentaController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Venta;
+use Illuminate\Http\Request;
+
+class VentaController extends Controller
+{
+    public function registrar(Request $request){
+        $request->validate([
+            //'idCliente' => 'required|exists:clientes,id',
+            'idProducto' => 'required|exists:productos,id',
+        ]);
+        $venta = new Venta();
+        $venta->idCliente = $request->idCliente;
+        $venta->idProducto = $request->idProducto;
+        $venta->cantidad = $request->cantidad;
+        $venta->estado = 1;
+        $venta->idcomprobante = 1; // por ahora
+        $venta->save();
+        return $venta; // para prueba
+    }
+
+    public function listar(){
+        return Venta::all();
+    }
+
+    public function obtener(string $id){
+        return Venta::findOrFail($id);
+    }
+
+    public function actualizar(Request $request, string $id){ // cambiar
+        $venta = Venta::findOrFail($id);
+        $venta->idCliente = $request->idCliente;
+        $venta->idProducto = $request->idProducto;
+        $venta->cantidad = $request->cantidad;
+        $venta->estado = 1;
+        $venta->comprobante = 1; // por ahora
+        $venta->save();
+        return $venta;
+    }
+
+    public function eliminar(string $id){
+        $venta = Venta::findOrFail($id);
+        $venta->estado = 0;
+        $venta->save();
+        //Venta::destroy($id);
+    }
+}
