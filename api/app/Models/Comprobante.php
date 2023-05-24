f765a5f79d32a0424ef6da080247a3d09e200927
kgmamanic <kgmamanic@unjbg.edu.pe>
2023-05-24 16:21:02 -0500

    Comprobante 40%

---
+++ api/app/Models/Comprobante.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comprobante extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'fecha_hora_creacion',
+        'fecha_hora_cancelacion',
+        'idServicio',
+        'idMetodo_pago'
+    ];
+}
---
+++ api/database/migrations/2023_05_24_044522_create_comprobantes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comprobantes', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('fecha_hora_creacion');
+            $table->datetime('fecha_hora_cancelacion')->nullable();
+            $table->unsignedInteger('idServicio');
+            $table->unsignedInteger('idMetodo_pago');
+            $table->timestamps();
+        });
+        Schema::create('tipos_servicio', function (Blueprint $table) {
+            $table->id();
+            $table->string('servicio');
+        });
+        Schema::create('metodos_pago', function (Blueprint $table) {
+            $table->id();
+            $table->string('metodo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comprobantes');
+    }
+};
