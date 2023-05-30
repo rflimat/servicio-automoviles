d29595dc7d18ef7c0dc73cf89fb8d4e9d0e219ea
rflimat <raulflimat@gmail.com>
2023-05-29 20:22:40 -0500

    Cambios en controlador ventas

--- api/app/Http/Controllers/api/VentaController.php
+++ api/app/Http/Controllers/api/VentaController.php
@@ -9,49 +9,64 @@ use Illuminate\Http\Request;
 class VentaController extends Controller
 {
     public function registrar(Request $request){
-        $request->validate([
+        /*$request->validate([
             //'idCliente' => 'required|exists:clientes,id',
             'idProducto' => 'required|exists:productos,id',
-        ]);
+        ]);*/
         
-        foreach($request->productosVenta as $producto){
+        foreach($request->productosVenta as $producto){    
             $venta = new Venta();
-            $venta->idCliente = $request->idCliente;
-            $venta->idProducto = $producto->idProducto;
-            $venta->cantidad = $producto->cantidad;
+            $venta->idCliente = $request["idCliente"];
+            $venta->idProducto = $producto["idProducto"];
+            $venta->cantidad = $producto["cantidadAct"];
             $venta->estado = 1;
             if($request->idComprobante){
-                $venta->idComprobante = $request->idComprobante;
+                $venta->idComprobante = $request["idComprobante"];
             }
-            $venta->importe = $producto->importe;
+            $venta->importe = $producto["importe"];
+            $venta->save();
         }
-        $venta->save();
-        return $venta; // para prueba
+        return 1; // para prueba
     }
 
     public function listar(){
-        $venta = Venta::where('estado', 1)->get();
+        $venta = Venta::select('idComprobante', 'idCliente', 'hora')
+        ->selectRaw('DATE_FORMAT(fecha, "%d/%m/%Y") as fecha')
+        ->selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as nombreCliente')
+        ->selectRaw('SUM(importe) as importe')
+        ->join('clientes', 'ventas.idCliente', '=', 'clientes.id')
+        ->where('estado', 1)
+        ->groupBy('idComprobante', 'idCliente', 'hora', 'fecha', 'nombreCliente')
+        ->orderBy('fecha', 'desc')
+        ->orderBy('hora', 'desc')
+        ->get();
         return $venta;
     }
     // es id del comprobante
-    public function obtener(string $id){
-        return Venta::select('idCliente','idProducto','cantidad','fecha','hora','idComprobante','importe')
-        ->where('idComprobante','=',$id);
+    public function obtener(string $id) {
+        $venta = Venta::select('idCliente', 'fecha', 'hora', 'idComprobante')
+        ->selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as nombreCliente')
+        ->join('clientes', 'ventas.idCliente', '=', 'clientes.id')
+        ->where('idComprobante', $id)
+        ->first();
+        $venta->productosVenta = Venta::select('ventas.id as idVenta', 'productos.id', 'productos.nombre', 'productos.cantidad', 'ventas.cantidad as cantidadAct', 'productos.precio_venta', 'ventas.importe')
+            ->join('productos', 'ventas.idProducto', '=', 'productos.id')
+            ->where('idComprobante', $id)->get();
+        $venta->costo_venta = Venta::where('idComprobante','=',$id)->sum('importe');
+        return $venta;
     }
 
     public function actualizar(Request $request, string $id){ // cambiar
         foreach($request->productosVenta as $producto){
-            $venta = Venta::findOrFail($producto->id);
-            $venta->cantidad = $producto->cantidad;
-            $venta->importe = $producto->importe;
+            $venta = Venta::findOrFail($producto["idVenta"]);
+            $venta->cantidad = $producto["cantidadAct"];
+            $venta->importe = $producto["importe"];
         }
         $venta->save();
         return $venta; // para prueba
     }
     public function eliminar(string $id){
-        $venta = Venta::findOrFail($id);
-        $venta->estado = 0;
-        $venta->save();
-        //Venta::destroy($id);
+        return Venta::where('idComprobante', $id)
+            ->update(['estado' => 0]);
     }
 }
