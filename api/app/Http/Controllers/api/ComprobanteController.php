8ee98e3fbe29770a0ae6658eba6bb1eeb700e34f
kgmamanic <kgmamanic@unjbg.edu.pe>
2023-07-01 02:36:33 -0500

    Update ComprobanteController.php

--- api/app/Http/Controllers/api/ComprobanteController.php
+++ api/app/Http/Controllers/api/ComprobanteController.php
@@ -77,53 +77,52 @@ class ComprobanteController extends Controller
             }
         }
         return $comprobantes;
-        /*
-        $comprobantes = Comprobante::select('comprobantes.id','nro_comprobante','fecha_hora_creacion','fecha_hora_cancelacion','idServicio','servicio','idMetodo_pago','metodo','estado','costo_total')
-            ->join('tipos_servicio','idServicio','=','tipos_servicio.id')
-            ->join('metodos_pago','idMetodo_pago','=','metodos_pago.id')
-            ->where('eliminado', 0)
-            ->get();
-        foreach($comprobantes as $comprobante){
-            $comprobante['ventas'] = Venta::select('ventas.id','fecha','hora','total_importe')
-                ->selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as nombreCliente')
-                ->join('clientes', 'ventas.idCliente', '=', 'clientes.id')
-                ->where('idComprobante', $comprobante->id)
-                ->where('estado','1')
-                ->get();
-            foreach($comprobante['ventas'] as $venta){
-                $venta['productosVenta'] = DetalleVenta::select('detalle_ventas.id','productos.nombre','detalle_ventas.cantidad','importe')
-                ->join('productos', 'detalle_ventas.idProducto', '=', 'productos.id')
-                ->where('detalle_ventas.idVenta', $venta['id'])
-                ->get();
-            }
-            // trabajos
-            $comprobante['trabajos'] = Trabajo::select('trabajos.id','idVehiculo','placa','problema_inicial','fecha_hora_ingreso','fecha_hora_salida','costo','estado','idTrabajador')
-                ->selectRaw('CONCAT(trabajadors.Nombres, " ", trabajadors.Apellidos) as nombreTrabajador')
-                ->join('trabajadors', 'trabajos.idTrabajador', '=', 'trabajadors.id')
-                ->join('vehiculos','trabajos.idVehiculo','=','vehiculos.id')
-                ->where('idComprobante', $comprobante->id)
-                ->where('eliminado','0')
-                ->get();
-            foreach($comprobante['trabajos'] as $trabajos){
-                $trabajos['detalleTrabajo'] = DetalleTrabajo::select('id','descripcion','fecha_hora','costo')
-                ->where('detalle_trabajos.idTrabajo', $trabajos['id'])
-                ->get();
-            }
-            
-        }
-        */
-        return $comprobantes;
     }
 
     public function obtener(string $id)
     {
         // falta
-        $comprobante = Comprobante::select('comprobantes.id','nro_comprobante','fecha_hora_creacion','fecha_hora_cancelacion','idServicio','servicio','idMetodo_pago','metodo','estado','costo_total')
+        $comprobante = Comprobante::select('comprobantes.id','nro_comprobante','comprobantes.fecha_hora_creacion','comprobantes.fecha_hora_cancelacion','idServicio',
+        'servicio','idMetodo_pago','metodo','comprobantes.estado','costo_total','idVenta','idTrabajo')
             ->join('tipos_servicio','idServicio','=','tipos_servicio.id')
             ->join('metodos_pago','idMetodo_pago','=','metodos_pago.id')
             ->where('comprobantes.id', $id)
             ->first();
+        if($comprobante->idServicio == 2 or $comprobante->idServicio == 3){
+            // caso en que sea venta o venta + trabajo
+            // se busca al cliente por la venta
+            $comprobante['cliente'] = Venta::selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as cliente')
+            ->join('clientes','clientes.id','=','idCliente')
+            ->where('ventas.id',$comprobante->idVenta)
+            ->first()->cliente;
+            // si es caso 3 entonces tambien incluye una placa
+            if($comprobante->idServicio == 3){
+                $comprobante['placa'] = Trabajo::join('vehiculos','vehiculos.id','=','idVehiculo')
+                    ->where('trabajos.id',$comprobante->idTrabajo)
+                    ->first()->placa;
+            }
+        }else{
+            // caso que solo sea trabajo y no venta
+            $comprobante['placa'] = Trabajo::join('vehiculos','vehiculos.id','=','idVehiculo')
+                ->where('trabajos.id',$comprobante->idTrabajo)
+                ->first()->placa;
+            $comprobante['cliente'] = Trabajo::selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as cliente')
+                ->join('vehiculos','vehiculos.id','=','idVehiculo')
+                ->join('clientes','clientes.id','=','cliente_id')
+                ->where('trabajos.id',$comprobante->idTrabajo)
+                ->first()->cliente;
+        }
 
+        $comprobante['productosVenta'] = DetalleVenta::select('detalle_ventas.id','nombre','detalle_ventas.cantidad','precio_venta','importe')
+            ->join('productos','productos.id','=','idProducto')
+            ->where('idVenta',$comprobante->idVenta)
+            ->get();
+        $comprobante['detalleTrabajo'] = DetalleTrabajo::select('id','descripcion','fecha_hora','costo')
+                ->where('idTrabajo',$comprobante->idTrabajo)->get();
+
+        return $comprobante;
+
+        /*
         $comprobante['ventas'] = Venta::select('ventas.id','fecha','hora','total_importe')
         ->selectRaw('CONCAT(clientes.Nombres, " ", clientes.Apellidos) as nombreCliente')
         ->join('clientes', 'ventas.idCliente', '=', 'clientes.id')
@@ -150,6 +149,7 @@ class ComprobanteController extends Controller
             ->get();
         }
         return $comprobante; // retorna
+        */
     }
     public function actualizar(Request $request, string $id)
     { // cambiar
